def2130c6a072bf5f9ca13416d8bb06bcdd5a61c
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-12-18 19:46:41 +0530

    fix: update import path for WindowsControls and add Home component to index export

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar.jsx";
 import Welcome from "./Welcome.jsx";
 import Dock from "./Dock.jsx";
-import WindowsControls from "#components/WindowsControls.jsx";
+import WindowsControls from "./WindowsControls.jsx";
+import Home from "./Home.jsx";
 
-export { Navbar, Welcome, Dock, WindowsControls };
+export { Navbar, Welcome, Dock, WindowsControls, Home };
